7f1d9f8fd2f3e27ce3190df08373e8297589e979
Shonda Warren <norshonda.warren@gmail.com>
2020-08-19 19:08:26 -0700

    border css video card

---
+++ tiktok-clone/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBsbpmmn222QZFcAsoETUhSMNnuE0y0sW8",
+  authDomain: "tiktok-clone-feded.firebaseapp.com",
+  databaseURL: "https://tiktok-clone-feded.firebaseio.com",
+  projectId: "tiktok-clone-feded",
+  storageBucket: "tiktok-clone-feded.appspot.com",
+  messagingSenderId: "288782923722",
+  appId: "1:288782923722:web:d535677215960369528133",
+  measurementId: "G-H03F8QWX7P"
+};
---
+++ tiktok-clone/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Video from './Video'
+import './App.css';
+
+function App() {
+  return (
+    //BEM 
+    <div className="app">
+     
+        <div className="app__videos">
+          <Video/>
+          <Video/>
+          <Video/>
+          <Video/>
+          <Video/>
+        </div>
+    </div>
+  );
+}
+
+export default App;
